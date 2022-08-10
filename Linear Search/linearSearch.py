650d8f407858961796402ed036895a533f5cee72
Sourin <98945276+heysourin@users.noreply.github.com>
2022-08-10 22:59:44 +0530

    Linear Search Using For Loop

---
+++ Linear Search/linearSearch.py
@@ -0,0 +1,12 @@
+def linearSearch(list1,e):
+    for x in range(len(list1)):
+        if e == list1[x]:
+            print('Hola peyechi')
+
+
+firstList = [12,13,14,15,16]
+
+key = int(input("Enter the number yoou're looking for: "))
+linearSearch(firstList,key)
+
+
