50a5969c71e933956282eca36fdc28a5681fe9d0
Sourin <98945276+heysourin@users.noreply.github.com>
2022-08-14 00:10:14 +0530

    Update BinarySearch.py

--- Binary Search/BinarySearch.py
+++ Binary Search/BinarySearch.py
@@ -12,13 +12,12 @@
 #5. start point will be the right next elemnent of the mid_indexdle element
 
 
-from turtle import right
 
 
-list1 = [12,13,14,15,16,23,45,77]
 
 #! Linear search:
 
+# list1 = [12,13,14,15,16,23,45,77]
 # TODO:process 1 
 
 # def linearSearch(list1,e):
@@ -26,18 +25,19 @@ list1 = [12,13,14,15,16,23,45,77]
 #         if e == list1[x]:
 #             print('Hola peyechi')
 
+
 # TODO:process 2- 'enupper_indexmerate' prints the index
-def linearSearch(list, n):
-    for i, e in enupper_indexmerate(list):
-        if e == n:
-            retupper_indexrn i
-    retupper_indexrn -1
+# def linearSearch(list, n):
+#     for i, e in enupper_indexmerate(list):
+#         if e == n:
+#             retupper_indexrn i
+#     retupper_indexrn -1
 
 
+# print(linearSearch(list1,106))
 
-print(linearSearch(list1,106))
 
-
+#! Binary search:
 
 def biSear(list, n):
     left_index = 0
@@ -49,7 +49,7 @@ def biSear(list, n):
         mid_number = list[mid_index]
 
         if mid_number == n:
-            print('Paisi')
+            return (mid_index)
             
         if mid_number < n:
             left_index = mid_index + 1
@@ -57,3 +57,10 @@ def biSear(list, n):
             upper_index = mid_index - 1
     
     return -1
+
+
+list2 = [12,13,14,15,16,23,45,77]
+index1 = biSear(list2, 485)
+index2 = biSear(list2, 45)
+print(f"Number found at {index1} using binary search")
+print(f"Number found at {index2} using binary search")
