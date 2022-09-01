563ed47ffc745551fecd836a7e1f35a2c1218388
Sourin <98945276+heysourin@users.noreply.github.com>
2022-09-01 14:22:11 +0530

    Create queue_list.py

---
+++ Queue/queue_list.py
@@ -0,0 +1,19 @@
+import queue
+
+
+# queue = []
+# queue.append(10)
+# queue.append(20)
+# queue.append(30)
+
+# queue.pop() #? remove the latest added element
+# queue.pop(0) #? removes 0th index element
+# print(queue)
+
+
+queue = []
+queue.insert(0,10)
+queue.insert(0,20)
+queue.insert(0,30)
+print(queue)
+print(not queue) #? checks empty or not
