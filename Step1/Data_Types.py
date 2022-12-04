20b49d4067f6d98798bf9cbb6be4163bd1017cdd
Sourin <98945276+heysourin@users.noreply.github.com>
2022-12-05 01:52:22 +0530

    Create Data_Types.py

---
+++ Step1/Data_Types.py
@@ -0,0 +1,20 @@
+# Various Data Types:
+#! NUMERIC Data Type:
+#int
+#float
+#complex
+#bool
+
+#! SEQUENCIAL Data Types: Mutable & Immutable
+#! Mutable Data Types: 
+#? -Which can be changes after creation(Readable + Writable)
+#? -Slower
+#List
+#Sets
+#Dictionary
+
+#!Immuutable Data Types: 
+#? -Which can not be changes after creation(Only readable)
+#Numbers
+#Strings
+#Tuples
