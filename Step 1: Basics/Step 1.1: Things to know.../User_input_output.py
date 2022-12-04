bc28f36dd18ff665270ca9b4206d084557b6a37f
Sourin <98945276+heysourin@users.noreply.github.com>
2022-12-05 01:31:03 +0530

    Create User_input_output.py

---
+++ Step 1: Basics/Step 1.1: Things to know.../User_input_output.py
@@ -0,0 +1,16 @@
+# x1 = input("enter a num: ")
+
+# #!By default python takes input in string
+# x2 = int(input("enter num2: "))
+
+# print (x1)
+# print (x2)
+
+
+#! You can even print input index wise:
+a1 = input("Enter a char: ")
+print(a1[0])
+
+#! If you want it to do evalution itself
+a2 = eval(input("Enter a char: "))#if input is 2+2 rather than a string
+print(a2)
