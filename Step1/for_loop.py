46e7758a5e63e76d7e03039d092c4b2b3b08ae82
Sourin <98945276+heysourin@users.noreply.github.com>
2022-12-09 00:49:46 +0530

    Create for_loop.py

---
+++ Step1/for_loop.py
@@ -0,0 +1,30 @@
+# x = ['navin', 67, 2.3, True]
+# for i in x:
+#     print(i)
+
+
+# x1 = "SOURIN"
+# for i in x1:
+#     print(i)
+
+
+# for i in [2, False, "Hello"]:
+#     print(i)
+
+
+#! for i in range(start, end, iteration)
+# for i in range(10):
+#     print(i)
+
+
+# for i in range(11,25,2):
+#     print(i)
+
+
+# for i in range(20,11, -2): #Todo: ***IMP***
+#     print(i)
+
+
+for i in range(1, 21):
+    if (i % 5 != 0):
+        print(i)
