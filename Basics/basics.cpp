2980d1718e7f664180fe476da060ac8e5186f1e2
Sourin <98945276+heysourin@users.noreply.github.com>
2022-12-21 01:29:22 +0530

    Create basics.cpp

---
+++ Basics/basics.cpp
@@ -0,0 +1,73 @@
+#include <iostream> //makes the program run, io stream library: for input and output
+// #include<bits/stdc++,h>
+using namespace std; // To replace 'std' in 5th line
+
+int main()
+{
+    //-------------Output-----------------//
+    // std::cout<<"hehe";
+    // cout<<"hello guys"<<"\n";//Used to create new line
+
+    // cout<<"hello guys"<<endl;//Used to create new line
+
+    //! Printing many lines in one line-code
+    // cout << "hello guys" << endl
+    //      << "Sab kyse ho?";
+
+    //-------------Taking input--------------
+    // int x, y;
+    // cin >> x >> y;
+    // cout << "Value of x is: " << x << " and y is: " << y;
+
+    //-------------Data Types-----------------//
+    //?int
+    // int x = 10;
+    //?long
+    // long x2 = 11;
+    //?long long
+    // long long x =1234567890987654321;
+
+    //?float, double, long double
+    // float x4= 5.6;
+    // double x5 = 6.43562456250692506825798;
+
+    //?string
+    // string s1;
+    // string s2;
+    // cin >> s1 >> s2;
+    // cout << s1 << " " << s2;
+    //** Taking input of string -whole line **//
+    // string s3;
+    // getline(cin, s3);
+    // cout<<"Your whole line is: "<<s3;
+
+    // //?char
+    // char ch = '2';//single quote used to declare a character~generally
+
+    //------------------if else----------------//
+    // int age;
+    // cin >> age;
+
+    // if (age >= 18)
+    // {
+    //     cout << "Adult";
+    // }
+    // else
+    // {
+    //     cout << "Kiddo";
+    // }
+    //***  Else statement is not mandatory
+    // if()
+    // {
+    // }
+    // else if()
+    // {
+    // }
+    // else
+    // {
+    // }
+
+    //** Else if saves time
+    //** elseif code when executed, it doesnot allow later part of the ifelse statement run
+    return 0;
+}
